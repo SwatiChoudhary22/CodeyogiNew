90d1df76ad42a645bf676a382110da84a451a001
yashu2019 <86866730+yashu2019@users.noreply.github.com>
2022-05-26 14:30:25 +0530

    first commit

---
+++ src/Button/Button.tsx
@@ -0,0 +1,10 @@
+import { FC, memo } from "react";
+type ButtonProps = {};
+
+const Button: FC<ButtonProps> = (props) => {
+  return <></>;
+};
+
+Button.defaultProps = {};
+
+export default memo(Button);
